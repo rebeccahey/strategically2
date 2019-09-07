371fab931ef1adaa90238ca3d18431331ada4ac5
rebecca <rebeccahey1001@gmail.com>
2019-09-07 18:15:10 +0100

    new gatsby page

---
+++ src/pages/web/Sectionsgatsby/OverviewSection.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+// react components for routing our app without refresh
+import { Link } from "gatsby";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+// @material-ui/icons
+
+// core components
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import Button from "components/CustomButtons/Button.jsx";
+import exampleStyle from "assets/jss/material-kit-react/views/componentsSections/exampleStyle.jsx";
+
+import landing from "assets/img/landing.jpg";
+import profile from "assets/img/profile.jpg";
+import alternate1 from "assets/img/alternate1.jpg";
+
+
+class OverviewSection extends React.Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.section}>
+        <div className={classes.container}>
+        <div css={{'@media (min-width: 300px) and (max-width: 800px)': {paddingLeft: '30px', paddingRight: '30px'}, position: 'relative', backgroundColor: '#fff', paddingTop: '30px'}}>
+          <GridContainer justify="center">
+            <GridItem xs={12} sm={12} md={6}>
+             <div style={{textAlign: "left"}}>
+             <h4>Gatsby JS Development</h4>
+             <p>Released in 2013, React is a powerful JavaScript library for building user interfaces. Developed by Facebook, it's popular with startups and Fortune 500 companies.
+             <p>It is fast, open-source and search engine friendly with components which can be reused. We love it because we can quickly build better products for our clients</p>
+             <h4>The React Framework</h4>
+             <p>The React framework means developers can reuse components on multiple pages on a website. This not only saves time, but also improves the cohesion of the user experience by standardising common elements.</p>
+             <p>Valuable time is saved, allowing developers to focus on application architecture and business logic. In addtion, one-way bindings make data flows easier to understand, spot issues and debug.</p>
+             <p>Web pages can render faster as the use of a Virutal dom means other JavaScript frameworks, like JQuery, are not required. As faster website load speeds are important SEO signals, this benefits the SEO of your website.</p>
+             <h4>React Native</h4>
+             <p>Use React Native to build mobile apps on iOS and Andriod. By using existing React code, you can launch quickly and have less code to maintain in the long term.</p>
+             <p>The time is takes to build mobile apps is significantly reduced by the ability to reuse elements of the same codebase for mobile and web applications.</p>
+             </p>
+             </div>
+          </GridItem>
+          </GridContainer>
+        </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withStyles(exampleStyle)(OverviewSection);
---
+++ src/pages/web/gatsbyPage.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// react components for routing our app without refresh
+import { Link } from "gatsby";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+// @material-ui/icons
+// core components
+import Header from "components/Header/Header.jsx";
+import Footer from "components/Footer/Footer.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import Button from "components/CustomButtons/Button.jsx";
+import Parallax from "components/Parallax/Parallax.jsx";
+// sections for this page
+import HeaderLinks from "components/Header/HeaderLinks.jsx";
+import OverviewSection from "./Sectionsgatsby/OverviewSection.jsx";
+import SectionExamples from "./Sectionsgatsby/SectionExamples.jsx";
+
+import componentsStyle from "assets/jss/material-kit-react/views/components.jsx";
+
+class Components extends React.Component {
+  render() {
+    const { classes, ...rest } = this.props;
+    return (
+      <div>
+      <Link to="./">
+      <Header
+        brand="Strategically"
+        rightLinks={<HeaderLinks />}
+        fixed
+        color="opaque"
+        changeColorOnScroll={{
+          height: 100,
+          color: "primary"
+        }}
+        {...rest}
+      />
+        </Link>
+        <Parallax image={require("assets/img/development_gatsby.jpg")}>
+          <div className={classes.container}>
+            <GridContainer>
+              <GridItem>
+              <h1 css={{'@media (min-width: 300px) and (max-width: 400px)': {textAlign: "left", paddingLeft: "25px", paddingRight: "40px",
+              fontSize: "40px"},
+              '@media (min-width: 400px) and (max-width: 800px)': {textAlign: "left", paddingLeft: "25px", paddingRight: "40px",
+              fontSize: "60px"},
+              '@media (min-width: 800px)': {textAlign: "left", paddingLeft: "200px", paddingRight: "360px", fontSize: "70px"}}}>Gatsby JS Development Company</h1>
+              <h3 css={{'@media (min-width: 300px) and (max-width: 800px)': {textAlign: "left", paddingLeft: "30px", paddingRight: "30px"}, '@media (min-width: 800px)': {textAlign: "left",
+              paddingLeft: "200px", paddingRight: "300px"}}}>
+              Based on React, Gatsby is a free and open source framework for building lightening fast websites and apps.
+              </h3>
+              </GridItem>
+            </GridContainer>
+          </div>
+        </Parallax>
+        <div className={classNames(classes.main, classes.mainRaised)}>
+        <OverviewSection />
+        <SectionExamples />
+    </div>
+        <div className={classes.conatinerCta}>
+        <GridContainer style={{ paddingTop: "80px", paddingBottom: "80px", backgroundColor: "#2A2C2D"}}>>
+          <GridItem>
+              <h1 css={{'@media (min-width: 300px) and (max-width: 800px)': {textAlign: "center", paddingLeft: "10px", paddingRight: "10px",
+              fontSize: "60px"}, '@media (min-width: 800px)': {textAlign: "center",
+              paddingLeft: "200px", paddingRight: "200px"}}}>Are you ready to 10X your growth?</h1>
+              <h3 css={{'@media (min-width: 300px) and (max-width: 800px)': {textAlign: "center", paddingLeft: "30px", paddingRight: "30px"}, '@media (min-width: 800px)': {textAlign: "center",
+              paddingLeft: "300px", paddingRight: "300px"}}}>
+              We help you grow faster with strategy, software and data, a whole lot of data.
+              </h3>
+              <Link to="./contact">
+              <Button color="primary" size="lg">
+                Get in Touch
+              </Button>
+              </Link>
+          </GridItem>
+        </GridContainer>
+        <Footer/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withStyles(componentsStyle)(Components);
