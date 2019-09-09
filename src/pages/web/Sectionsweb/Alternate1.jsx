24f9bde90fc9a88d4f0fc8163920966e08bcc45e
rebecca <rebeccahey1001@gmail.com>
2019-09-09 17:35:09 +0100

    web page import fix three

--- src/pages/web/Sectionsweb/Alternate1.jsx
+++ src/pages/web/Sectionsweb/Alternate1.jsx
@@ -22,22 +22,22 @@ class Alternate1 extends React.Component {
   render() {
     const { classes } = this.props;
     return (
+      <div css={{'@media (min-width: 300px) and (max-width: 800px)': {paddingLeft: '30px', paddingRight: '30px'}, position: 'relative', backgroundColor: '#fff', paddingTop: '30px'}}>
       <div css={{padding: "0px!important"}} className={classes.section}>
-        <div css={{'@media (min-width: 300px) and (max-width: 800px)':
+        <div css={{'@media (min-width: 300px) and (max-width: 2000px)':
           {padding: "0px!important"},backgroundColor: "#fff", padding: "0px!important"}} className={classes.section}>
           <GridContainer justify="center">
             <GridItem xs={12} sm={12} md={6}>
               <div css={{'@media (min-width: 300px) and (max-width: 800px)': {paddingLeft: "20px", paddingRight: "20px", paddingBottom: "0px"}, '@media (min-width: 800px)': {paddingLeft: "70px"}}}>
-                <h2 css={{'@media (min-width: 300px) and (max-width: 800px)': {textAlign: "center", fontSize: "2.3rem", marginTop: "50px"}}} >We grow B2B brands with data-backed strategies</h2>
-                <h6 css={{'@media (min-width: 300px) and (max-width: 800px)': {textAlign: "center"}, '@media (min-width: 800px)': {textAlign: "left"}}}>_______________</h6>
-                <h4 css={{'@media (min-width: 300px) and (max-width: 800px)': {textAlign: "center"}}} >01: SEO Strategy</h4>
-                <h5 css={{'@media (min-width: 300px) and (max-width: 800px)': {textAlign: "center"}}}>We save you time and money by providing you with a data-backed strategy, so you can stop guessing and start growing.</h5>
+                <h2 css={{'@media (min-width: 300px) and (max-width: 800px)': {textAlign: "center", fontSize: "2.3rem", marginTop: "50px"}}} >Backend Web Development</h2>
+                <h6 css={{'@media (min-width: 300px) and (max-width: 800px)': {textAlign: "center"}, '@media (min-width: 800px)': {textAlign: "left"}}}>_________</h6>
+                <h5 css={{'@media (min-width: 300px) and (max-width: 800px)': {textAlign: "center"}}}>Rock-solid, reliable backend development ensures your business logic works as it should.</h5>
                 <div css={{'@media (min-width: 300px) and (max-width: 800px)': {textAlign: "center"}, '@media (min-width: 800px)': {textAlign: "left"}}}>
-                  <Link to="./seo">
-                  <Button css={{'@media (min-width: 300px) and (max-width: 800px)': {fontSize: "1.7rem"}}} color="primary" size="lg">
-                Learn more
-                </Button>
-                </Link>
+              <Link to="./development/node">
+              <Button css={{'@media (min-width: 300px) and (max-width: 800px)': {fontSize: "1.7rem", marginRight: "0px!important"}}} color="primaryweb" size="lg">
+            Node JS
+            </Button>
+            </Link>
                 </div>
                </div>
             </GridItem>
@@ -48,6 +48,7 @@ class Alternate1 extends React.Component {
             </GridItem>
           </GridContainer>
         </div>
+      </div>
       </div>
     );
   }
