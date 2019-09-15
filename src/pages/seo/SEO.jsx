57ae9e0fa55692be413cb0ef6ec5c18e5805d410
rebecca <rebeccahey1001@gmail.com>
2019-09-15 20:08:05 +0100

    meta for seo page

--- src/pages/seo/SEO.jsx
+++ src/pages/seo/SEO.jsx
@@ -31,7 +31,7 @@ class Components extends React.Component {
       <div>
       <Helmet>
       <title>Next Level Search Engine Optimisation| Strategically.co</title>
-      <meta name="description" content="Audience-focused organic search engine marketing" />
+      <meta name="description" content="A lot of people have a website or a developer, but no real plan. You don't need more features or a shiny new front end. What you need is a plan. Our clients have one — do you?" />
       </Helmet>
       <Link to="./">
       <Header
