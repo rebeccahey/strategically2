b4e3b7c1d3d2ddbb161269425718776e973e0a1a
rebecca <rebeccahey1001@gmail.com>
2019-09-17 21:01:13 +0100

    install google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,3 +25,10 @@ module.exports = {
     'gatsby-plugin-offline',
   ],
 }
+{
+   resolve: `gatsby-plugin-google-analytics`,
+   options: {
+     trackingId: "UA-127625937-1",
+     head: "true",
+   },
+ },
