4b5a9b988a2bc5f6a44f857b5fe1a86947e49ae0
rebecca <rebeccahey1001@gmail.com>
2019-09-17 14:41:33 +0100

    deleting unused code 4

--- src/pages/siteevaluationPage/siteevaluationPage.jsx
+++ src/pages/siteevaluationPage/siteevaluationPage.jsx
@@ -1,80 +1,77 @@
 import React from "react";
-// nodejs library that concatenates classes
-import classNames from "classnames";
 // react components for routing our app without refresh
 import { Link } from "gatsby";
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
 // @material-ui/icons
+
 // core components
-import Header from "components/Header/Header.jsx";
-import Footer from "components/Footer/Footer.jsx";
 import GridContainer from "components/Grid/GridContainer.jsx";
 import GridItem from "components/Grid/GridItem.jsx";
 import Button from "components/CustomButtons/Button.jsx";
-import Parallax from "components/Parallax/Parallax.jsx";
-// sections for this page
-import HeaderLinks from "components/Header/HeaderLinks.jsx";
-import OverviewSection from "./Sections/OverviewSection.jsx";
-import SectionExamples from "./Sections/SectionExamples.jsx";
+import exampleStyle from "assets/jss/material-kit-react/views/componentsSections/exampleStyle.jsx";
 
-import componentsStyle from "assets/jss/material-kit-react/views/components.jsx";
+import blog1 from "assets/img/blog1.jpg";
+import blog2 from "assets/img/blog2.jpg";
 
-class Components extends React.Component {
+class SectionExamples extends React.Component {
   render() {
-    const { classes, ...rest } = this.props;
+    const { classes } = this.props;
     return (
-      <div>
-      <Header
-        brand="Strategically"
-        rightLinks={<HeaderLinks />}
-        fixed
-        color="opaque"
-        changeColorOnScroll={{
-          height: 100,
-          color: "primary"
-        }}
-        {...rest}
-      />
-        <Parallax image={require("assets/img/ServiceImage.jpg")}>
-          <div className={classes.container}>
-            <GridContainer>
-              <GridItem>
-              <h1 css={{'@media (min-width: 300px) and (max-width: 400px)': {textAlign: "left", paddingLeft: "25px", paddingRight: "40px",
-              fontSize: "40px"},
-              '@media (min-width: 400px) and (max-width: 800px)': {textAlign: "left", paddingLeft: "25px", paddingRight: "40px",
-              fontSize: "60px"},
-              '@media (min-width: 800px)': {textAlign: "left", paddingLeft: "200px", paddingRight: "360px", fontSize: "70px"}}}>SEO Site Evaluation That Goes Beyond Typical SEO</h1>
-              </GridItem>
-            </GridContainer>
-          </div>
-        </Parallax>
-        <div className={classNames(classes.main, classes.mainRaised)}>
-        <OverviewSection />
-        <SectionExamples />
-    </div>
-        <div className={classes.conatinerCta}>
-        <GridContainer style={{ paddingTop: "80px", paddingBottom: "80px", backgroundColor: "#2A2C2D"}}>>
-          <GridItem>
-              <h1 css={{'@media (min-width: 300px) and (max-width: 800px)': {textAlign: "center", paddingLeft: "10px", paddingRight: "10px",
-              fontSize: "60px"}, '@media (min-width: 800px)': {textAlign: "center",
-              paddingLeft: "200px", paddingRight: "200px"}}}>Are you ready to 10X your growth?</h1>
-              <h3 css={{'@media (min-width: 300px) and (max-width: 800px)': {textAlign: "center", paddingLeft: "30px", paddingRight: "30px"}, '@media (min-width: 800px)': {textAlign: "center",
-              paddingLeft: "300px", paddingRight: "300px"}}}>
-              We help you grow faster with strategy, software and data, a whole lot of data.
-              </h3>
-              <Link to="./contact">
-              <Button color="primary" size="lg">
-                Get in Touch
-              </Button>
-              </Link>
-          </GridItem>
-        </GridContainer>
-        <Footer/>
+      <div className={classes.section}>
+        <div className={classes.container}>
+          <GridContainer>
+            <GridItem xs={12} sm={12} md={6}>
+              <Link to="./blog/seo-strategy">
+                <img
+                  src={blog1}
+                  alt="SEO keyword research"
+                  className={
+                    classes.imgBlog
+                  }
+                  css={{'@media (min-width: 300px) and (max-width: 800px)':{width: '100%!important'},'@media (min-width: 800px)':{width: 'auto!important'}}}
+                />
+                </Link>
+                <div style={{ textAlign: "center"}}>
+                <h4 css={{'@media (min-width: 300px) and (max-width: 800px)': {paddingLeft: "20px",paddingRight: "20px",paddingTop: "15px",
+                textAlign: "center"}, '@media (min-width: 800px)': {paddingLeft: "70px",
+                paddingRight: "70px",paddingTop: "15px",textAlign: "center"}}}>The 7 steps to a killer SEO strategy</h4>
+                <h5 css={{'@media (min-width: 300px) and (max-width: 800px)': {paddingLeft: "30px",paddingRight: "30px",textAlign: "center"}, '@media (min-width: 800px)': {paddingLeft: "100px",paddingRight: "100px",textAlign: "center"}}}>An SEO strategy is a data-backed blueprint to generate pre-qualified leads to your website using organic traffic.</h5>
+                <Link to="./blog/seo-strategy">
+                <Button color="primary" size="lg">
+                    Read More
+                </Button>
+                </Link>
+                </div>
+            </GridItem>
+            <GridItem xs={12} sm={12} md={6}>
+              <Link to="./blog/seo-strategy/#analysing-the-competitive-landscape">
+              <img
+                  src={blog2}
+                  alt="Competitive analysis for SEO"
+                  className={
+                    classes.imgBlog
+                  }
+                  css={{'@media (min-width: 300px) and (max-width: 800px)':{width: '100%!important'},'@media (min-width: 800px)':{width: 'auto!important'}}}
+                />
+                </Link>
+                <div style={{ textAlign: "center"}}>
+                <h4 css={{'@media (min-width: 300px) and (max-width: 800px)': {paddingLeft: "20px",paddingRight: "20px",paddingTop: "15px",
+                textAlign: "center"}, '@media (min-width: 800px)': {paddingLeft: "70px",
+                paddingRight: "70px",paddingTop: "15px",textAlign: "center"}}}>Analysing the competitive landscape</h4>
+                <h5 css={{'@media (min-width: 300px) and (max-width: 800px)': {paddingLeft: "30px",paddingRight: "30px",textAlign: "center"}, '@media (min-width: 800px)': {paddingLeft: "100px",paddingRight: "100px",textAlign: "center"}}}>Learn where your business stands in a competitive landscape, giving you the power to market your strength.</h5>
+                <Link to="./blog/seo-strategy/#analysing-the-competitive-landscape">
+                <Button color="primary" size="lg">
+                    Read More
+                </Button>
+                </Link>
+                </div>
+            </GridItem>
+          </GridContainer>
         </div>
       </div>
     );
   }
 }
 
-export default withStyles(componentsStyle)(Components);
+export default withStyles(exampleStyle)(SectionExamples);
