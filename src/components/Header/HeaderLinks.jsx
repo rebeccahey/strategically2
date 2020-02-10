e7054351e335d6c7d953abbf46543bb130d0cfac
Radi Danchev <rdanchev@gmail.com>
2020-02-10 17:36:26 +0200

    WIP: work page

--- src/components/Header/HeaderLinks.jsx
+++ src/components/Header/HeaderLinks.jsx
@@ -1,137 +1,171 @@
 /*eslint-disable*/
-import React from "react";
-import classNames from "classnames";
+import React from "react"
+import classNames from "classnames"
 
 // react components for routing our app without refresh
-import { Link } from "gatsby";
+import { Link } from "gatsby"
 
 // @material-ui/core components
-import withStyles from "@material-ui/core/styles/withStyles";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import Tooltip from "@material-ui/core/Tooltip";
+import withStyles from "@material-ui/core/styles/withStyles"
+import List from "@material-ui/core/List"
+import ListItem from "@material-ui/core/ListItem"
+import Tooltip from "@material-ui/core/Tooltip"
 
 // @material-ui/icons
-import { Apps, CloudDownload } from "@material-ui/icons";
+import { Apps, CloudDownload } from "@material-ui/icons"
 
 // React icons
-import { FaTwitter, FaFacebook, FaInstagram } from 'react-icons/fa';
+import { FaTwitter, FaFacebook, FaInstagram } from "react-icons/fa"
 
 // core components
-import CustomDropdown from "components/CustomDropdown/CustomDropdown.jsx";
-import Button from "components/CustomButtons/Button.jsx";
+import CustomDropdown from "components/CustomDropdown/CustomDropdown.jsx"
+import Button from "components/CustomButtons/Button.jsx"
 
-import headerLinksStyle from "assets/jss/material-kit-react/components/headerLinksStyle.jsx";
+import headerLinksStyle from "assets/jss/material-kit-react/components/headerLinksStyle.jsx"
 
 function HeaderLinks({ ...props }) {
-  const { classes } = props;
+  const { classes } = props
   return (
     <List className={classes.list}>
-    <ListItem className={classes.listItem}>
-    <CustomDropdown
-      noLiPadding
-      buttonText="Development"
-      buttonProps={{
-        className: classes.navLink,
-        color: "transparent"
-      }}
-      dropdownList={[
-        <Link to="./development" className={classes.dropdownLink}>
-          Software Development
-        </Link>,
-        <Link to="./development/node" className={classes.dropdownLink}>
-          Node JS Development
-        </Link>,
-        <Link to="./development/react" className={classes.dropdownLink}>
-          React JS Development
-        </Link>,
-        <Link to="./development/gatsby" className={classes.dropdownLink}>
-          Gatsby JS Development
-        </Link>
-      ]}
-    />
-    </ListItem>
-    <ListItem className={classes.listItem}>
-    <CustomDropdown
-      noLiPadding
-      buttonText="SEO"
-      buttonProps={{
-        className: classes.navLink,
-        color: "transparent"
-      }}
-      dropdownList={[
-        <Link to="./seo" className={classes.dropdownLink}>
-          Search Engine Optimisation
-        </Link>,
-        <Link to="./seo/audit" className={classes.dropdownLink}>
-          SEO Site Audit
-        </Link>,
-        <Link to="./seo/strategy" className={classes.dropdownLink}>
-          SEO Content Strategy
-        </Link>,
-        <Link to="./seo/in-house" className={classes.dropdownLink}>
-          In-House SEO Setup
-        </Link>
-      ]}
-    />
-    </ListItem>
-    <ListItem className={classes.listItem}>
-    <CustomDropdown
-      noLiPadding
-      buttonText="Content Strategy"
-      buttonProps={{
-        className: classes.navLink,
-        color: "transparent"
-      }}
-      dropdownList={[
-        <Link to="./strategy" className={classes.dropdownLink}>
-          Content Services
-        </Link>,
-        <Link to="./strategy/content" className={classes.dropdownLink}>
-          Content Strategy
-        </Link>,
-        <Link to="./strategy/content-marketing" className={classes.dropdownLink}>
-          Content Marketing Strategy
-        </Link>
-      ]}
-    />
-    </ListItem>
-    <ListItem className={classes.listItem}>
-    <CustomDropdown
-      noLiPadding
-      buttonText="Competitive Analysis"
-      buttonProps={{
-        className: classes.navLink,
-        color: "transparent"
-      }}
-      dropdownList={[
-        <Link to="./intelligence" className={classes.dropdownLink}>
-          Competitive Analysis
-        </Link>,
-        <Link to="./intelligence/competition" className={classes.dropdownLink}>
-          Competitive Landscape
-        </Link>,
-        <Link to="./intelligence/audience" className={classes.dropdownLink}>
-          Audience Targeting
-        </Link>,
-      ]}
-    />
-  </ListItem>
+      <ListItem className={classes.listItem}>
+        <CustomDropdown
+          noLiPadding
+          buttonText="Work"
+          buttonProps={{
+            className: classes.navLink,
+            color: "transparent",
+          }}
+          dropdownList={[
+            <Link to="./work" className={classes.dropdownLink}>
+              SaaS
+            </Link>,
+            <Link to="./work#seo" className={classes.dropdownLink}>
+              SEO
+            </Link>,
+            <Link to="./work#cro" className={classes.dropdownLink}>
+              CRO
+            </Link>,
+            <Link to="./work#social" className={classes.dropdownLink}>
+              Social
+            </Link>,
+          ]}
+        />
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <CustomDropdown
+          noLiPadding
+          buttonText="Development"
+          buttonProps={{
+            className: classes.navLink,
+            color: "transparent",
+          }}
+          dropdownList={[
+            <Link to="./development" className={classes.dropdownLink}>
+              Software Development
+            </Link>,
+            <Link to="./development/node" className={classes.dropdownLink}>
+              Node JS Development
+            </Link>,
+            <Link to="./development/react" className={classes.dropdownLink}>
+              React JS Development
+            </Link>,
+            <Link to="./development/gatsby" className={classes.dropdownLink}>
+              Gatsby JS Development
+            </Link>,
+          ]}
+        />
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <CustomDropdown
+          noLiPadding
+          buttonText="SEO"
+          buttonProps={{
+            className: classes.navLink,
+            color: "transparent",
+          }}
+          dropdownList={[
+            <Link to="./seo" className={classes.dropdownLink}>
+              Search Engine Optimisation
+            </Link>,
+            <Link to="./seo/audit" className={classes.dropdownLink}>
+              SEO Site Audit
+            </Link>,
+            <Link to="./seo/strategy" className={classes.dropdownLink}>
+              SEO Content Strategy
+            </Link>,
+            <Link to="./seo/in-house" className={classes.dropdownLink}>
+              In-House SEO Setup
+            </Link>,
+          ]}
+        />
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <CustomDropdown
+          noLiPadding
+          buttonText="Content Strategy"
+          buttonProps={{
+            className: classes.navLink,
+            color: "transparent",
+          }}
+          dropdownList={[
+            <Link to="./strategy" className={classes.dropdownLink}>
+              Content Services
+            </Link>,
+            <Link to="./strategy/content" className={classes.dropdownLink}>
+              Content Strategy
+            </Link>,
+            <Link
+              to="./strategy/content-marketing"
+              className={classes.dropdownLink}
+            >
+              Content Marketing Strategy
+            </Link>,
+          ]}
+        />
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <CustomDropdown
+          noLiPadding
+          buttonText="Competitive Analysis"
+          buttonProps={{
+            className: classes.navLink,
+            color: "transparent",
+          }}
+          dropdownList={[
+            <Link to="./intelligence" className={classes.dropdownLink}>
+              Competitive Analysis
+            </Link>,
+            <Link
+              to="./intelligence/competition"
+              className={classes.dropdownLink}
+            >
+              Competitive Landscape
+            </Link>,
+            <Link to="./intelligence/audience" className={classes.dropdownLink}>
+              Audience Targeting
+            </Link>,
+          ]}
+        />
+      </ListItem>
       <ListItem className={classes.listItem}>
         <Link to="/contact">
           <Button
             color="transparent"
             target=""
-            css={{'@media (min-width: 300px) and (max-width: 800px)': {color: "#4b5665!important"}, '@media (min-width: 800px)': {color: "#fff!important"}}}
+            css={{
+              "@media (min-width: 300px) and (max-width: 800px)": {
+                color: "#4b5665!important",
+              },
+              "@media (min-width: 800px)": { color: "#fff!important" },
+            }}
             className={classes.navLink}
           >
-           Contact
+            Contact
           </Button>
         </Link>
-
       </ListItem>
     </List>
-  );
+  )
 }
 
-export default withStyles(headerLinksStyle)(HeaderLinks);
+export default withStyles(headerLinksStyle)(HeaderLinks)
---
+++ src/pages/work.js
@@ -0,0 +1,129 @@
+import React from "react"
+import withStyles from "@material-ui/core/styles/withStyles"
+
+import componentsStyle from "assets/jss/material-kit-react/views/components.jsx"
+
+import Header from "components/Header/Header.jsx"
+import Footer from "components/Footer/Footer.jsx"
+import HeaderLinks from "components/Header/HeaderLinks.jsx"
+import GridContainer from "components/Grid/GridContainer.jsx"
+import GridItem from "components/Grid/GridItem.jsx"
+import Box from "@material-ui/core/Box"
+import Button from "@material-ui/core/Button"
+import BlogPost from "components/BlogPost"
+import BlogCategory from "components/BlogCategory"
+
+import blog1 from "assets/img/blog1.jpg"
+import blog2 from "assets/img/blog2.jpg"
+
+const StyledButton = withStyles({
+  root: {
+    backgroundColor: "#CACACA",
+    borderRadius: 3,
+    border: 0,
+    color: "#000",
+    height: 48,
+    padding: "0 30px",
+    "&:hover": {
+      color: "#FFF",
+      backgroundColor: "#e83e33",
+      borderColor: "#e83e33",
+      boxShadow: "none",
+    },
+  },
+  label: {
+    textTransform: "capitalize",
+  },
+})(Button)
+
+const Work = ({ classes, ...rest }) => (
+  <>
+    <Header
+      brand="Strategically"
+      rightLinks={<HeaderLinks />}
+      fixed
+      color="primary"
+      {...rest}
+    />
+    <GridContainer justify="center" alignItems="center" spacing={3}>
+      <GridItem xs={12}>
+        <Box mt={"108px"} maxWidth="800px" px={3} py={8} mx="auto">
+          <h1
+            css={{
+              fontSize: "48px",
+              color: "#000",
+            }}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </h1>
+          <p css={{ textAlign: "center" }}>
+            Movet diceret lucilius quo at, at his causae nonumes. No menandri
+            postulant eam, ius id tale oratio mentitum. In mea doctus utroque
+            luptatum.
+          </p>
+        </Box>
+      </GridItem>
+
+      <GridContainer
+        spacing={3}
+        direction="row"
+        justify="center"
+        alignItems="center"
+        style={{ marginTop: "16px", marginBottom: "24px"}}
+      >
+        <GridItem xs={6} md={3}>
+          <StyledButton href="#saas">SaaS</StyledButton>
+        </GridItem>
+        <GridItem xs={6} md={3}>
+          <StyledButton href="#seo">SEO</StyledButton>
+        </GridItem>
+        <GridItem xs={6} md={3}>
+          <StyledButton href="#cro">CRO</StyledButton>
+        </GridItem>
+        <GridItem xs={6} md={3}>
+          <StyledButton href="#social">Social</StyledButton>
+        </GridItem>
+      </GridContainer>
+
+      <BlogCategory title="SaaS" sectionID="saas" />
+      <BlogPost
+        title="What Exactly Does Customer Experience (CX) Mean? And How Can We Measure It?"
+        excerpt="Movet diceret lucilius quo at, at his causae nonumes. No menandri
+        postulant eam, ius id tale oratio mentitum. In mea doctus utroque
+        luptatum."
+        linkURL="https://www.abtasty.com/blog/what-is-cx-how-to-measure-it/"
+        imageURL="/assets/images/blog1.jpg"
+        logoURL="/assets/images/abtasty-logo-1.svg"
+      />
+      <BlogPost
+        title="A sample blog post title"
+        excerpt="Movet diceret lucilius quo at, at his causae nonumes. No menandri
+        postulant eam, ius id tale oratio mentitum. In mea doctus utroque
+        luptatum."
+        imageURL="/assets/images/blog2.jpg"
+        logoURL="/assets/images/abtasty-logo-1.svg"
+      />
+
+      <BlogCategory title="SEO" sectionID="seo" />
+      <BlogPost
+        title="A sample blog post title"
+        excerpt="Movet diceret lucilius quo at, at his causae nonumes. No menandri
+        postulant eam, ius id tale oratio mentitum. In mea doctus utroque
+        luptatum."
+        imageURL="/assets/images/blog2.jpg"
+        logoURL="/assets/images/abtasty-logo-1.svg"
+      />
+      <BlogPost
+        title="A sample blog post title"
+        excerpt="Movet diceret lucilius quo at, at his causae nonumes. No menandri
+        postulant eam, ius id tale oratio mentitum. In mea doctus utroque
+        luptatum."
+        imageURL="/assets/images/blog1.jpg"
+        logoURL="/assets/images/abtasty-logo-1.svg"
+      />
+    </GridContainer>
+    <Footer />
+  </>
+)
+
+export default withStyles(componentsStyle)(Work)
