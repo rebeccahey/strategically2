5c380f78e62d57dcc0c9e649829effaa1475a723
rebecca <rebeccahey1001@gmail.com>
2020-02-23 18:29:10 +0000

    add excerpts to blog page

---
+++ src/components/BlogPost2.jsx
@@ -0,0 +1,64 @@
+import React from "react"
+import GridItem from "components/Grid/GridItem.jsx"
+import Box from "@material-ui/core/Box"
+import Button from "components/CustomButtons/Button.jsx"
+import { Link } from "gatsby"
+
+const BlogPost2 = ({ title, excerpt, imageURL, imageAlt, logoURL, logoAlt, linkURL }) => (
+  // <a href={linkURL} target="_blank" rel="noreferrer noopener">
+    <GridItem xs={12} md={6} textAlign="left">
+      <Box maxWidth="500px" mx="auto" textAlign="left" px={3}>
+
+        <img
+          src={imageURL}
+          alt={imageAlt}
+          css={{
+            maxWidth: "100%",
+            height: "auto",
+            "@media (min-width: 300px) and (max-width: 800px)": {
+              width: "100%!important",
+            },
+            "@media (min-width: 800px)": { width: "auto!important" },
+          }}
+        />
+        <img
+          src={logoURL}
+          alt={logoAlt}
+          css={{
+            marginTop: "14px",
+            height: "40px",
+            width: "auto",
+          }}
+        />
+        <Box
+          component="h3"
+          fontWeight="fontWeightBold"
+          fontSize={24}
+          my={2}
+          color="#000"
+          textAlign="left"
+        >
+          {title}
+        </Box>
+        <Box component="p" fontSize={18}>
+                  {excerpt}
+                </Box>
+        <Link to={linkURL}>
+                      <Button
+                        css={{
+                          "@media (min-width: 300px) and (max-width: 800px)": {
+                            fontSize: "1.7rem",
+                          },
+                        }}
+                        color="primary"
+                        size="lg"
+
+                      >
+                        Read Article
+                      </Button> </Link>
+      </Box>
+    </GridItem>
+  // </a>
+)
+
+export default BlogPost2
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -10,7 +10,7 @@ import GridItem from "components/Grid/GridItem.jsx"
 import Box from "@material-ui/core/Box"
 import Button from "@material-ui/core/Button"
 import RegularButton from "components/CustomButtons/Button.jsx"
-import BlogPost from "components/BlogPost"
+import BlogPost2 from "components/BlogPost2"
 import BlogCategory from "components/BlogCategory"
 import { Link } from "gatsby"
 
@@ -55,44 +55,52 @@ const Work = ({ classes, ...rest }) => (
           B2B & SaaS Content Marketing Blog </h1>
     </Box>
       </GridItem>
-      <BlogPost
+      <BlogPost2
         title="The 7 Steps To A Killer SEO Strategy"
+        excerpt="An effective SEO strategy organises your website’s content and architecture by topic to support your business goals, meet your customer needs and inform Google of a users searching intent."
         linkURL="./blog/seo-strategy"
         imageURL="/assets/images/seo1.jpg"
       />
-      <BlogPost
+      <BlogPost2
         title="Business Goals, Audiences & Value Props"
+        excerpt="Optimization is more than just meta tags and links. Search is a marketing channel, and that channel caters to particular audiences with specific needs to be met while also fulfilling your website goals."
         linkURL="./blog/seo-strategy#understanding-business-goals-audiences-and-value-propositions"
         imageURL="/assets/images/seo2.jpg"
       />
-      <BlogPost
+      <BlogPost2
         title="How to Do In-Depth Keyword Research"
+        excerpt="The purpose of keyword research is to determine topics relevant to what you provide, the best keywords to target and incorporate, and to prioritize content and topics based on relevance and search volume."
         linkURL="./blog/seo-strategy#keyword-research"
         imageURL="/assets/images/seo3.jpg"
       />
-      <BlogPost
+      <BlogPost2
         title="Conducting Opportunity & Gap Analysis"
-        linkURL="https://strategically.co/blog/seo-strategy#opportunity--gap-analysis"
+        excerpt="For existing businesses a gap and opportunity analysis is an excellent way to use Google search volume data and your existing traffic data to get a clear picture of your website's strengths and weaknesses."
+        linkURL="./blog/seo-strategy#opportunity--gap-analysis"
         imageURL="/assets/images/seo4.jpg"
       />
-      <BlogPost
+      <BlogPost2
         title="Who Are Your Competitors?"
-        linkURL="https://strategically.co/blog/seo-strategy#who-are-your-competitors"
+        excerpt="If we know which businesses rank in the top spots, we can dissect the strategies and techniques they employ to get there. We then have a really clear understanding what it takes to rank your website and beat the competition."
+        linkURL="./blog/seo-strategy#who-are-your-competitors"
         imageURL="/assets/images/seo5.jpg"
       />
-      <BlogPost
+      <BlogPost2
         title="Analysing the Competitive Landscape"
-        linkURL="https://strategically.co/blog/seo-strategy#analysing-the-competitive-landscape"
+        excerpt="If your goal is to be ranked in the top 5 on Google for the categories relevant to your business, then you need to dissect what, why, and how the other guys are doing it. That’s where competitive analysis comes in."
+        linkURL="./blog/seo-strategy#analysing-the-competitive-landscape"
         imageURL="/assets/images/seo6.jpg"
       />
-      <BlogPost
+      <BlogPost2
         title="SEO Strategy & Recommendations"
-        linkURL="https://strategically.co/blog/seo-strategy#seo-strategy--recommendations"
+        excerpt="Now it’s time to pull it all together into an actionable strategy. It may sound like a daunting task, in this step, we will round up all the data from steps 1–5 and create a document of strategic SEO recommendations."
+        linkURL="./blog/seo-strategy#seo-strategy--recommendations"
         imageURL="/assets/images/seo7.jpg"
       />
-      <BlogPost
+      <BlogPost2
         title="Prioritisation and Cost Benefit Analysis"
-        linkURL="https://strategically.co/blog/seo-strategy#Prioritisation-and-cost-benefit-analysis"
+        excerpt="The goal of a strategy is not to overload a business. It is to provide actionable steps, in order of priority, to take your business from its current position to reigning supreme on the web."
+        linkURL="./blog/seo-strategy#Prioritisation-and-cost-benefit-analysis"
         imageURL="/assets/images/seo8.jpg"
       />
     </GridContainer>
