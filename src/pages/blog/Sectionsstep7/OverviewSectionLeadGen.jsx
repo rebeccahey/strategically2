bcc5c2a4ffd4456edc583457fd031c9ba72bf093
rebecca <rebeccahey1001@gmail.com>
2020-02-25 13:58:24 +0000

    copy change on blog

--- src/pages/blog/Sectionsstep7/OverviewSectionLeadGen.jsx
+++ src/pages/blog/Sectionsstep7/OverviewSectionLeadGen.jsx
@@ -21,27 +21,26 @@ class OverviewSectionLeadGen extends React.Component {
             <GridItem xs={12} sm={12} md={6}>
              <div style={{textAlign: "left"}}>
              <h1 style={{ color: "#000", textAlign: "left", fontSize: "65px"}}>How to Generate Leads for a Consulting Business</h1>
-<p>Regardless of if you run a small freelance consulting firm or you work for McKinsey, lead generation is a critical function of growing the business.</p>
-<p>The volume of leads of each consulting firm will vary greatly depending on your niche and the power of your brand in a given niche. At <a href="http://strategically.co/">Strategically</a> we focus on creating and promoting high-quality leads. </p>
-<p>Even for specific niche services and well-known brands in these sectors, firms will still require a significant number of leads to achieve a single signed contract. This is the nature of sales. </p>
-<p>The quality of the leads you generate will also vary greatly depending on the lead source and the tactics used to generate marketing-qualified leads. </p>
-<p>In our opinion, the highest quality leads are from customers outwardly searching for your services. Generally, these leads are acquired by either organic or paid traffic. </p>
-<p>However, the total volume of this type of lead available in highly specialised consulting niches can be very limited, so additional lead volume must be acquired to support the sale efforts. </p>
-<p>In this article we are going to discuss some of the tactics that can be used to generate leads in the consulting niche. </p>
+             <p>Whatever your business size or industry, lead generation is a critical function of growing a consulting business. All firms, even well-known brands in small niches, require a significant number of leads to achieve a single signed contract. That’s the nature of sales.</p>
+             <p>The volume of leads available for each consulting firm will vary greatly depending on your niche and the power of your brand in your industry. The quality of the leads will vary greatly depending on the lead source and the tactics used to generate marketing-qualified leads.</p>
+             <p>The highest quality leads are from customers outwardly searching for your services. They are much closer to making a decision and if your website solves their pain, the conversion is far easier. Generally, these leads are acquired by either organic or paid traffic.</p>
+             <p>However, the total volume of this type of lead available in highly specialised consulting niches can be very limited, so additional lead volume must be acquired to support the sale efforts.</p>
+             <p>At Strategically we focus on creating and promoting high-quality leads. In this article, we will discuss our best lead generation tactics for consulting firms to help you reach your marketing goals.</p>
 <h2 style={{ fontSize: "40px",}}>Find Customers Looking for Services With Pay Per Click</h2>
-<p>Almost certainly, paid Google adverts should be part of the lead generation strategy for a consulting firm. For consulting lead generation Google Ads campaigns, we almost always opt for a campaign structure known as a SKAG (Single Keyword Advert Group) campaign to maximise the potential click-through rate and outperform standard PPC campaigns.</p>
+<p>Paid Google adverts should be part of the lead generation strategy for a consulting firm. For consulting lead generation Google Ads campaigns, we (nearly) always opt for a campaign structure known as a SKAG (Single Keyword Advert Group) campaign to maximise the potential click-through rate and outperform standard PPC campaigns.</p>
 <img style={{ width: `100%`,}}
                src="/assets/images/Lead_Generation_For_Consulting_Companies.jpg" alt="lead generation for consulting companies"></img>
 <h2 style={{ fontSize: "40px",}}>Create Service Landing Pages to Capture Organic Search</h2>
-<p>Traffic quality matters. Most consulting firms work in small niches where the traffic volume is limited. The highest quality traffic available are users searching directly for consulting services. We often recommend listing the core areas of expertise in the main navigation and then creating a mega navigation to allow users to drill down into the specific services on offer. Creating a single landing page tailored to each microservice you offer helps the website rank for long-tail keywords and helps potential clients understand the company's areas of expertise.</p>
+<p>Traffic quality matters. Most consulting firms work in small niches where the traffic volume is limited. The highest quality traffic is made up of users searching directly for specific consulting services. Create a single landing page tailored to each microservice you offer not only helps the website rank for long-tail keywords but also helps potential clients understand the company's areas of expertise.</p>
 <img style={{ width: `100%`,}}
                    src="/assets/images/Lead_generation_for_consulting_companies1.jpg" alt="lead generation for consulting companies"></img>
 <h2 style={{ fontSize: "40px",}}>Create More Valuable Blog Content</h2>
 <p>The importance of creating regular high-quality content cannot be underestimated. Pay close attention to what people are searching for and create content that your customers will find valuable. Most firms should be creating between four and eight new items of high-quality content per month.</p>
 <img style={{ width: `100%`,}}
                    src="/assets/images/Lead_gen_for_consulting_companies.jpg" alt="lead generation for consulting companies"></img>
-<h2 style={{ fontSize: "40px",}}>Create More Downloadable Content</h2>
-<p>Creating a knowledge hub is a must when looking to generate consulting leads. When creating this resource you should consider creating a diverse selection of downloadable guides, as the diversity will increase the chances of your content aligning with the needs of your potential clients. The greater the alignment between the audience's needs and the resources, the greater the chance the users will download your content.&nbsp;Creating downloadable resources helps to generate potential consulting clients early in the sales cycle as the prospects are generally looking for information around the subject months prior to searching for a vendor. The earlier your sales team can connect with prospects, the more time you have to build the relationship with the prospects.</p>
+<h2 style={{ fontSize: "40px",}}>Create Downloadable Content</h2>
+<p>Creating a knowledge hub is an excellent way to generate consulting leads. You should consider creating a broad selection of downloadable guides, as the diversity will increase the chances of your content aligning with the needs of your potential clients. The greater the alignment between the audience's needs and the resources, the greater the chance the users will download your content. </p>
+<p>What’s more, creating downloadable resources helps to generate potential consulting clients early in the sales cycle. Prospects at this stage in the funnel are researching information around the subject months prior to searching for a vendor. The earlier your sales team can connect with prospects, the more time you have to build the relationship.</p>
 <img style={{ width: `100%`,}}
                    src="/assets/images/Lead_gen_for_consulting_firms.png" alt="lead generation for consulting companies"></img>
 <h2 style={{ fontSize: "40px",}}>Build an Audience Using Social Media Groups</h2>
@@ -53,11 +52,11 @@ class OverviewSectionLeadGen extends React.Component {
 <img style={{ width: `100%`,}}
                    src="/assets/images/Lead_gen_consulting_firms.jpg" alt="lead generation for consulting companies"></img>
 <h2 style={{ fontSize: "40px",}}>Create an Online Demo that Takes the Pressure Off Your Sales Team</h2>
-<p>If you are selling software as part of your consulting service, then an online demo locked behind a lead form can be a great way to connect with people who want to know more about your services but who are not quite ready to speak with the sales team. This group of potential customers can be sizeable, and with a good CRM follow-up process, the rewards can be significant.</p>
+<p>If you sell software as part of your consulting service, an online demo locked behind a lead form can be a great way to connect with people who want to know more about your services but who are not quite ready to speak with the sales team. This group of potential customers can be sizeable, and with a good CRM follow-up process, the rewards can be significant.</p>
 <img style={{ width: `100%`,}}
                    src="/assets/images/Consulting_lead_generation.jpg" alt="lead generation for consulting companies"></img>
 <h2 style={{ fontSize: "40px",}}>Try Tender Portals</h2>
-<p>Find out which portals are applicable for your niche and ensure that you are submitting tenders. For smaller consulting firms this could be looking at online freelance websites such as Upwork, while for large consulting companies you might be tendering for national government contracts on large specialised tender platforms.</p>
+<p>Find out which portals are applicable for your niche and submit tenders on a regular basis. For smaller consulting firms this could include online freelance websites such as Upwork, while larger consulting companies could tender for national government contracts on specialised tender platforms.</p>
 <img style={{ width: `100%`,}}
                    src="/assets/images/Lead_generation_for_consulting_companies2.jpg" alt="lead generation for consulting companies"></img>
 <h2 style={{ fontSize: "40px",}}>Outbound Sales</h2>
@@ -65,7 +64,7 @@ class OverviewSectionLeadGen extends React.Component {
 <img style={{ width: `100%`,}}
                    src="/assets/images/Lead_generation_for_consulting_companies_3.jpg" alt="lead generation for consulting companies"></img>
 <h2 style={{ fontSize: "40px",}}>Use Adroll, Facebook or LinkedIn Retargeting to Drive Returning Visitors</h2>
-<p>Promoting content and adverts at your custom audiences can be a very effective tactic to assist your conversions. It is very rare that a user will convert on their first visit to the website, so help your customers remember your brand after the initial touch by using adverts and promoted content.&nbsp;&nbsp;</p>
+<p>Promoting content and adverts at your custom audiences can be a very effective tactic to assist your conversions. It is rare that a user will convert on their first visit to your website, so help your customers remember your brand after the initial touch by using adverts and promoted content.</p>
 <img style={{ width: `100%`,}}
                    src="/assets/images/lead_generation_for_consulting_companies_4.jpg" alt="lead generation for consulting companies"></img>
 <h2 style={{ fontSize: "40px",}}>Create More Case Studies</h2>
@@ -73,17 +72,25 @@ class OverviewSectionLeadGen extends React.Component {
 <img style={{ width: `100%`,}}
                    src="/assets/images/Lead_generation_f0r_consulting_companies_5.png" alt=""></img>
 <h2 style={{ fontSize: "40px",}}>Create a Glossary of Terms Which Relate to Your Niche</h2>
-<p>Creating a glossary of terms might seem like a waste of energy; however, this simple tactic can generate a lot of quality links which will help your content rank. The key to building a glossary that works is to create a single page per term using the following URL structure: www.example.com/glossary/glossary-term/. This ensures that every glossary term page is highly specific for the search query.</p>
+<p>Creating a glossary of terms might seem like a waste of energy; however, this simple tactic can generate lots of quality links which will help your content rank. The key to building a glossary that works is to create a single page per term using the following URL structure: www.example.com/glossary/glossary-term/. This ensures that every glossary term page is highly specific for the search query.</p>
 <img style={{ width: `100%`,}}
                    src="/assets/images/lead_generation_for_consulting_companies_6.jpg" alt="lead generation for consulting companies"></img>
 <h2 style={{ fontSize: "40px",}}>When You Are Selling Knowledge, Your People Matter</h2>
-<p>Consulting is all about selling knowledge. Your people are your greatest asset and the website should showcase your people front and centre. Make sure you link back from your email footer to your LinkedIn and profile page to help potential clients reach out to you when you send emails.</p>
+<p>Consulting is about selling knowledge. Your people are your greatest asset and the website should showcase your people front and centre. Make sure you link back from your email footer to your LinkedIn and profile page to help potential clients reach out to you when you send emails.</p>
 <img style={{ width: `100%`,}}
                    src="/assets/images/lead_generation_for_consulting_companies_7.jpg" alt="lead generation for consulting companies"></img>
 <h2 style={{ fontSize: "40px",}}>Make Sure You Set Up Analytics Correctly</h2>
 <p>Understanding Google Analytics data can make the difference between success and failure. Analytics should be part of your weekly or monthly lead generation process. Use cohort analysis to evaluate the performance of your company's lead generation performance week over week.</p>
 <img style={{ width: `100%`,}}
                    src="/assets/images/lead_generation_for_consulting_companies_8.png" alt="lead generation for consulting companies"></img>
+<h2 style={{ fontSize: "40px",}}>Now It’s Over to You</h2>
+
+<p>That’s our simple guide to generating leads for consulting businesses. </p>
+<p>Which of the techniques are you going to focus on first in your lead generation plan?</p>
+<p>Do you need to revisit your Google Adwords campaigns? </p>
+<p>Or maybe you need to focus on creating content? </p>
+<p>Either way, let us know how you get on or reach out if you need any assistance. </p>
+
 </div>
     </GridItem>
           </GridContainer>
